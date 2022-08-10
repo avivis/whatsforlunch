6edc88fab3fa1a3c431edb31a9c1e00aa27871ad
Avighna Suresh <67458389+avivis@users.noreply.github.com>
2022-08-10 14:12:15 -0700

    Update search.js

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,5 +1,5 @@
 function search(search_url="/search?", search_value=document.getElementById('search_value').value, zip_code=document.getElementById('zip_code')) {
-    let xhr = $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + zip_code + '&key=' + config['AIzaSyBaFmWC5h6ehbNeZPbbhMm2G9S2hF2QgRU']);
+    let xhr = $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + zip_code + '&key=' + config['GOOGLE_API_KEY']);
     xhr.done(function(data) {
         var position = {
             'lat': data.results[0].geometry.location.lat,
