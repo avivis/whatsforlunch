8013558e428a669c7e692fcc6ce2550d1d60c7af
Avighna Suresh <67458389+avivis@users.noreply.github.com>
2022-08-10 14:06:27 -0700

    Create search.js

---
+++ static/scripts/search.js
@@ -0,0 +1,22 @@
+function search(search_url="/search?", search_value=document.getElementById('search_value').value, zip_code=document.getElementById('zip_code')) {
+    let xhr = $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + zip_code + '&key=' + config['AIzaSyBaFmWC5h6ehbNeZPbbhMm2G9S2hF2QgRU']);
+    xhr.done(function(data) {
+        var position = {
+            'lat': data.results[0].geometry.location.lat,
+            'long': data.results[0].geometry.location.lng,
+        };
+        getCall(position, search_url, search_value)
+    })
+}
+
+function getCall(position, url, search_value) {
+    var lat;
+    var long;
+    
+    lat = position['lat'];
+    long = position['long'];
+
+    let data = {"lat": lat, "long": long, "search_value": search_value};
+    let queryString = $.param(data);
+    window.location.replace(url + queryString);
+}
